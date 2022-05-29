0565065d2ec31e48f283ebd1f48984a67fa44ff6
zhangw1 <zhangw1@getui.com>
2022-05-30 00:13:24 +0800

    homework lesson12

---
+++ src/main/java/com/will/geetbang/lesson12/src/test/java/io/kimmking/javacourse/mq/activemq/ActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.geetbang.lesson12.src.test.java.io.kimmking.javacourse.mq.activemq;//package io.kimmking.javacourse.mq.activemq;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ActivemqApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
